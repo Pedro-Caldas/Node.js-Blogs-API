748e51e772426a3fc32355875012b5c3ce48f802
Pedro Caldas <pedronerislc@gmail.com>
2022-08-25 17:06:59 -0300

    Feat: create model blogPost

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   User.associate = (models) => {
-    User.hasMany(models.BlogPosts, {
+    User.hasMany(models.BlogPost, {
       foreignKey: "userId",
       as: "Users"
     });
