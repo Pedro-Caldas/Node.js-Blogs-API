0edae95a1db9c60c173c093e22ef5cb7f6cf22b0
Pedro Caldas <pedronerislc@gmail.com>
2022-08-25 17:16:34 -0300

    Feat: create model postCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'PostCategories'
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      as: 'BlogPosts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      as: 'Categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  }
+
+  return PostCategory;
+};
